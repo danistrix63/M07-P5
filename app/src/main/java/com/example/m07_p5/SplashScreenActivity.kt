0c7296ac92b185b65a531d28865dcfd00b31a39e
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-01-26 22:27:19 +0100

    errores solucionados + splash funcional

--- app/src/main/java/com/example/m07_p5/SplashScreenActivity.kt
+++ app/src/main/java/com/example/m07_p5/SplashScreenActivity.kt
@@ -1,23 +1,25 @@
-/*
- * SplashScreenActivity.kt
- * Pantalla de inicio con logotipo
- */
 package com.example.m07_p5
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val logo = findViewById<ImageView>(R.id.logo)
+        val anim = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        logo.startAnimation(anim)
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000) // 2 segundos de duración
+        }, 2000)
     }
 }
